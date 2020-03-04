130335d3540a58446f312db8bc12b9239dd29028
Abel Zambrano Anacleto <37409439+Artego7@users.noreply.github.com>
2020-03-04 09:48:58 +0100

    Update

--- Vertical/Assets/Scripts/CameraControl/CamDummy.cs
+++ Vertical/Assets/Scripts/CameraControl/CamDummy.cs
@@ -13,7 +13,12 @@ public class CamDummy : MonoBehaviour
     void Update()
     {
         transform.position = player.position + cameraSO.positionDummy;
+<<<<<<< HEAD
+        transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * Time.deltaTime * (cameraSO.delay * 30f));
+        transform.RotateAround(transform.position, Vector3.forward, Time.deltaTime * (cameraSO.delay * 30f));
+=======
         transform.RotateAround(transform.position, Vector3.forward, Time.deltaTime * (cameraSO.delay * 30f));
 
+>>>>>>> 8946b0cb6be74cbda7e99576b32dc271a4455a96
     }
 }
